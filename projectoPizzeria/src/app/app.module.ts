7efc5f76b10d73756b96f3bd40f55369b9cd500b
Brayan <brayan.poolxd@gmail.com>
2021-06-20 22:54:08 -0500

    Componente header creado

--- projectoPizzeria/src/app/app.module.ts
+++ projectoPizzeria/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
 import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemComponent,
-    MainComponent
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
