b24f09cbe1962541763fcef96644923b4e9a4347
kevin <kevin.santiago.27@unsch.edu.pe>
2021-07-15 21:02:29 -0500

    ModalsValidaciones

--- projectoPizzeria/src/app/app.component.ts
+++ projectoPizzeria/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
+import { PersonaServicio } from './persona.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'projectoPizzeria';
   text!: string;
+
+  personas: Persona[] = [];
+  constructor(private personaServicio: PersonaServicio) {
+    this.personas = personaServicio.personas;
+  }
+
+  validarRegistro(): boolean {
+    let existeUsuario: boolean = false;
+    for (let i = 0; i < this.personas.length - 1; i++) {
+      if (this.personas[i].numeroDni === this.personas[i + 1].numeroDni) {
+        existeUsuario = true;
+      }
+    }
+    return existeUsuario;
+  }
+
+  logearCuenta(correo: string, contrasenia: string) {
+    let existeCuenta: boolean = false;
+    for (let i = 0; i < this.personas.length - 1; i++) {
+      if (
+        this.personas[i].correo === correo &&
+        this.personas[i].contrasenia === contrasenia
+      ) {
+        existeCuenta = true;
+      }
+    }
+    return existeCuenta;
+  }
 }
