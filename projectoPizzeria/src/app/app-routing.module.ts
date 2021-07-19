28bea88b25fbdbc15a6ad0859b64ece86afb8db2
Lucho <luis.quicano.27@unsch.edu.pe>
2021-07-18 23:56:08 -0500

    Por qué no está lo de Amado wtf

--- projectoPizzeria/src/app/app-routing.module.ts
+++ projectoPizzeria/src/app/app-routing.module.ts
@@ -1,22 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CarritoListComponent } from './carrito-list/carrito-list.component';
+import { ItemListComponent } from './item-list/item-list.component';
 import { DefaultComponent } from './layouts/default/default.component';
+import { LocalesComponent } from './locales/locales.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { PostsComponent } from './modules/posts/posts.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 
 
-const routes: Routes = [{
-  path: '',
-  component: DefaultComponent,
-  children: [{
+const routes: Routes = [
+  {
     path: '',
-    component: DashboardComponent
-  }, {
-    path: 'posts',
-    component: PostsComponent
-  }]
-}];
+    component: ItemListComponent
+  },
+  {
+    path: 'admin',
+    component: DefaultComponent,
+    children: [{
+      path: 'admin',
+      component: DashboardComponent
+    }, {
+      path: 'posts',
+      component: PostsComponent
+    }]
+  },
+  {
+    path: 'about',
+    component: NosotrosComponent
+  },
+  {
+    path: 'locales',
+    component: LocalesComponent
+  },
+  {
+    path: 'cart',
+    component: CarritoListComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
